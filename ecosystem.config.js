ee8dff086d132995a731b428aab21bcefe71ea7e
hyperneutrino <me@hyperneutrino.xyz>
2025-10-20 13:59:40 -0400

    use pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+export const apps = [
+    {
+        name: "discord:main",
+        script: "bun run discord:main",
+    },
+    {
+        name: "discord:customs",
+        script: "bun run discord:customs",
+    },
+];
